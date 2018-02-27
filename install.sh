b8ce691d15d4e52b8577360f9f4b22312c128c3c
koromodako <koromodako@gmail.com>
2018-02-27 20:11:27 +0100

    Fix: install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 # -!- encoding:utf8 -!-
 
+function print {
+    echo '[install.sh]> ${1}'
+}
+
 function check_cmd {
     if ! [ -x "$(command -v ${1})" ]; then
-        echo '[install.sh]> install ${1} and try again.' >&2
+        print 'install ${1} and try again.'
         exit 1
     fi
 }
 
+print 'check for mandatory commands'
 check_cmd git
 check_cmd pip3
-
+print 'creating ~/bin if inexistant'
 mkdir -p ~/bin
-
+print 'entering ~/bin'
 cd ~/bin
-
+print 'cloning repository'
 git clone https://github.com/pdautry/mkctf
-
+print 'installing Python requirements'
 pip3 install -r mkctf/requirements.txt
-
-ln -s mkctf/mkctf.py mkctf
-
+print 'creating symlink'
+ln -s mkctf/mkctf.py ~/bin/mkctf
+print 'exiting ~/bin'
 cd ~/
