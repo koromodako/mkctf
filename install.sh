ab3669294a41363125395572ed2c97e269a3192e
Paul Dautry <paul.dautry@gmail.com>
2018-02-27 21:06:26 +0100

    Fix: install script again :(

--- install.sh
+++ install.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 # -!- encoding:utf8 -!-
 
+WD=$(pwd)
+BIN_DIR=~/bin
+REPO_DIR=${BIN_DIR}/mkctf
+
 function print {
-    echo '[install.sh]> ${1}'
+    echo "[install.sh]> ${1}"
 }
 
 function check_cmd {
     if ! [ -x "$(command -v ${1})" ]; then
-        print 'install ${1} and try again.'
+        print "install ${1} and try again."
         exit 1
     fi
 }
 
-print 'check for mandatory commands'
+print "check for mandatory commands"
 check_cmd git
 check_cmd pip3
-print 'creating ~/bin if inexistant'
-mkdir -p ~/bin
-print 'entering ~/bin'
-cd ~/bin
-print 'cloning repository'
-git clone https://github.com/pdautry/mkctf
-print 'installing Python requirements'
-pip3 install -r mkctf/requirements.txt
-print 'creating symlink'
-ln -s mkctf/mkctf.py ~/bin/mkctf
-print 'exiting ~/bin'
-cd ~/
+print "creating ${BIN_DIR} if inexistant"
+mkdir -p ${BIN_DIR}
+print "entering ${BIN_DIR}"
+cd ${BIN_DIR}
+if [[ -d "${REPO_DIR}" ]]; then
+    print "pulling repository"
+    cd ${REPO_DIR}
+    git pull
+    cd ${BIN_DIR}
+else
+    print "cloning repository"
+    git clone https://github.com/pdautry/mkctf
+fi
+print "installing Python requirements"
+pip3 install -r ${REPO_DIR}/requirements.txt
+print "creating symlink"
+ln -s ${REPO_DIR}/mkctf.py ${BIN_DIR}/mkctf-cli
+print "going back to ${WD}"
+cd ${WD}
