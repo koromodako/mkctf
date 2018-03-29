315a3ebdef77306a5246e1e2434f5e73a9d44bb7
Paul Dautry <paul.dautry@gmail.com>
2018-03-29 19:19:43 +0200

    Add setup.py

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 # -!- encoding:utf8 -!-
 
 WD=$(pwd)
-BIN_DIR=~/bin
-CONF_DIR=~/.config
-REPO_DIR=${BIN_DIR}/mkctf
+INST_DIR=~/.local
+REPO_DIR=/tmp/mkctf
 
 function print {
     echo "[install.sh]> ${1}"
@@ -20,32 +19,23 @@ function check_cmd {
 print "check for mandatory commands"
 check_cmd git
 check_cmd pip3
-
-print "creating ${BIN_DIR} if inexistant"
-mkdir -p ${BIN_DIR}
-
-print "entering ${BIN_DIR}"
-cd ${BIN_DIR}
+check_cmd python3
 
 if [[ -d "${REPO_DIR}" ]]; then
     print "pulling repository"
     cd ${REPO_DIR}
     git pull
-    cd ${BIN_DIR}
 else
     print "cloning repository"
+    cd /tmp
     git clone https://github.com/pdautry/mkctf
 fi
 
 print "installing Python requirements"
 pip3 install -r ${REPO_DIR}/requirements.txt
 
-print "creating symlink"
-ln -s ${REPO_DIR}/mkctf.py ${BIN_DIR}/mkctf-cli
-
-print "creating configuration"
-mkdir -p ${CONF_DIR}
-cp ${REPO_DIR}/mkctf.yml.dist ${CONF_DIR}/mkctf.yml
+print "running setup.py"
+python3 setup.py install --prefix ${INST_DIR}
 
 print "going back to ${WD}"
 cd ${WD}
